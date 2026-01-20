00ff46971b6a66203937a9fc6673fd038102cfa5
Parth Bhutaiya <180641325+Parth-Bhutaiya-06@users.noreply.github.com>
2026-01-20 09:19:03 +0530

    Add files via upload
    HTML CSS JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Login successful (demo)");
+});
+
+document.getElementById("postGigForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Gig posted successfully (demo)");
+});
