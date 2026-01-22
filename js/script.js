bf6a0f0507b253cabaab1151cecc6715892755ae
Parth Bhutaiya <180641325+Parth-Bhutaiya-06@users.noreply.github.com>
2026-01-22 21:56:57 +0530

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Login successful (demo)");
-});
-
-document.getElementById("postGigForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Gig posted successfully (demo)");
-});
